59913b52c870d2112929d22c9e9a98dab0e5a71c
Daniel <dany_ivan_gr@hotmail.com>
2021-04-18 13:06:57 -0500

    fix: change data to js object and add facts

---
+++ dino.js
@@ -0,0 +1,117 @@
+dinos = [
+        {
+            "species": "Triceratops",
+            "weight": 13000,
+            "height": 114,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "facts": ["First discovered in 1889 by Othniel Charles Marsh",
+                "Triceratops means three-horned-face in Greek.",
+                "The triceratops' head is one of the largest of any land animal discovered.",
+                "Some triceratops may have had as many as 800 teeth!",
+                "They are a member of the ceratopsia suborder of dinosaurs.",
+                "It probably wasn't a very fast dinosaur."
+        ]
+        },
+        {
+            "species": "Tyrannosaurus Rex",
+            "weight": 11905,
+            "height": 144,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "facts": ["The largest known skull measures in at 5 feet long.",
+                "Tyrannosaurus rex is one of the largest dinosaurs to have walked the earth.",
+                "It lived 70 million years ago in what is now North America.",
+                "This dinosaur’s massive jaws were lined with 50-60 serrated teeth, some measuring 9 inches long. ",
+                "Its 4-foot-long jaws were big enough to swallow an adult human!",
+                "The name Tyrannosaurus Rex comes from Greek and Latin words meaning ‘Tyrant Lizard King’.",
+                "The T. Rex had a brain twice as big as those of the other giant carnivores."
+        ]
+        },
+        {
+            "species": "Anklyosaurus",
+            "weight": 10500,
+            "height": 55,
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "facts": ["Anklyosaurus survived for approximately 135 million years.",
+                "The name Ankylosaurus means ‘fused lizard’.",
+                "Ankylosaurus lived at the end of the Cretaceous Period (around 66 million years ago).",
+                "Ankylosaurus were herbivores (plant eaters) and had small teeth relative to their body size.",
+                "The Ankylosaurus had a massive tail club that it could use to generate a large amount of force.",
+                "The signature feature of Ankylosaurus was its body armor."
+        ]
+        },
+        {
+            "species": "Brachiosaurus",
+            "weight": 70000,
+            "height": "372",
+            "diet": "herbavor",
+            "where": "North America",
+            "when": "Late Jurasic",
+            "facts": ["An asteroid was named 9954 Brachiosaurus in 1991.",
+                "The meaning of their name is 'arm reptiles'",
+                "You pronounce their name 'brack-ee-ow-sor-us'.",
+                "It was discovered in 1903 by American paleontologist Elmer S. Riggs.",
+                "The Brachiosaurus probably fed mostly on foliage above five meters (16 feet) high",
+                "The Brachiosaurus may have travelled in herds"
+        ]
+        },
+        {
+            "species": "Stegosaurus",
+            "weight": 11600,
+            "height": 79,
+            "diet": "herbavor",
+            "where": "North America, Europe, Asia",
+            "when": "Late Jurasic to Early Cretaceous",
+            "facts": ["The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+                "Stegosaurus had a brain the size of a walnut.",
+                "Paleontologists once thought stegosaurus had a brain in its butt.",
+                "The spiked tail of stegosaurus is called a 'thagomizer'.",
+                "Stegosaurus supplemented its diet with small rocks.",
+                "Stegosaurus was one of the earliest dinosaurs to evolve cheeks."
+            ]
+        },
+        {
+            "species": "Elasmosaurus",
+            "weight": 16000,
+            "height": 59,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "facts": ["Elasmosaurus was a marine reptile first discovered in Kansas.",
+                "You pronounce their name 'ee-lazmo-saw-rus'.",
+                "The meaning of its name is 'Thin Plated Lizard'.",
+                "They lived during the Late Cretaceous Period.",
+                "They were slow swimmers.",
+                "the first fossil of elasmosaurus was discovered in kansas."
+        ]
+        },
+        {
+            "species": "Pteranodon",
+            "weight": 44,
+            "height": 20,
+            "diet": "carnivor",
+            "where": "North America",
+            "when": "Late Cretaceous",
+            "facts": ["Actually a flying reptile, the Pteranodon is not a dinosaur.",
+                "Pteranodon means 'Winged and Toothless'",
+                "Pteranodon was a flying reptile that lived during the time of the dinosaurs ",
+                "it was not a dinosaur, but was a close relative of the dinosaurs",
+                "Pteranodon's wing-span is longer than that of any known bird.",
+                "It had a crest on its head, no teeth at all, and a very short tail."
+        ]
+        },
+        {
+            "species": "Pigeon",
+            "weight": 0.5,
+            "height": 9,
+            "diet": "herbavor",
+            "where": "World Wide",
+            "when": "Holocene",
+            "facts": "All birds are living dinosaurs."
+        }
+]
